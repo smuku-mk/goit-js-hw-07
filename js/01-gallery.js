4a16ccbd89dc865f5d6eb32575a26d881c2f7aba
Mateusz Kubiak <smuku.mk@gmail.com>
2023-05-08 12:54:03 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const list = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<li class="gallery__item">
+        <a class="gallery__link" href="${image.original}">
+          <img
+            src="${image.preview}" 
+            data-source="${image.original}"
+            alt="${image.description}" 
+            class="gallery__image"
+          />
+        </a>
+      </li>`
+  )
+  .join("");
+list.insertAdjacentHTML("afterbegin", markup);
 
-console.log(galleryItems);
+const photoClick = (event) => {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}"width="800" height="600">`);
+  instance.show();
+};
+
+list.addEventListener("click", photoClick);
